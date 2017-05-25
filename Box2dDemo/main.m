cdeeb593b40668e9c9d2893282dc111ffbf054a4
songjufeng <songjufeng@mobike.com>
2017-05-25 18:42:24 +0900

    空白工程

---
+++ Box2dDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Box2dDemo
+//
+//  Created by 宋炬峰 on 2017/5/25.
+//  Copyright © 2017年 宋炬峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
